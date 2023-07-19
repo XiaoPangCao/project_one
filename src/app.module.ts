e20d63e21a3e0eb71df7587fffe5c0c13a397c6f
caoyinming <924856768@qq.com>
2023-07-19 14:35:15 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { GlobalConfig } from "./utils/GlobalMethods";
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { databaseConfig } from './utils/database'
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       ignoreEnvFile: true
-    })],
+    }),
+    TypeOrmModule.forRoot(databaseConfig),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService,],
 })
---
+++ src/utils/database.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { getConfig } from './index'
+const { MYSQL } = getConfig()
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: MYSQL.host,
+  port: MYSQL.port,
+  username: MYSQL.username,
+  password: MYSQL.password,
+  database: MYSQL.database,
+  synchronize: MYSQL.synchronize,
+  logging: MYSQL.logging,
+  entities: MYSQL.entities,
+  autoLoadEntities: MYSQL.autoLoadEntities,
+  // 其他选项...
+};
